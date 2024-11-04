1410f2a63c4931236df2f595c6e32d7af8e74cc3
csu李 <783450901@qq.com>
2024-11-04 16:09:03 +0800

    add onnx output and simplify

--- NanoTrack/pytorch2onnx.py
+++ NanoTrack/pytorch2onnx.py
@@ -17,7 +17,7 @@ parser = argparse.ArgumentParser(description='lighttrack')
 
 parser.add_argument('--config', type=str, default='./models/config/configv3.yaml',help='config file')
 
-parser.add_argument('--snapshot', default='models/pretrained/nanotrackv3.pth', type=str,  help='snapshot models to eval')
+parser.add_argument('--snapshot', default='models/snapshot/coco_tank_allDatasets.pth', type=str,  help='snapshot models to eval')
 
 args = parser.parse_args()
 
@@ -40,13 +40,18 @@ def main():
     head_net = model.ban_head
     
     # backbone input-xf
-    backbone_x = torch.randn([1, 3, 255, 255], device=device)
-    export_onnx_file_path= './models/onnx/nanotrack_backbone.onnx'
-    torch.onnx.export(backbone_net, backbone_x, export_onnx_file_path, input_names=['input'], output_names=['output'], verbose=True, opset_version=14)   
+    backbone_x = torch.randn([1, 3, 127, 127], device=device)
+    export_onnx_file_path= './models/onnx/backbone127.onnx'
+    torch.onnx.export(backbone_net, backbone_x, export_onnx_file_path, input_names=['Tinput'], output_names=['Toutput'], verbose=True, opset_version=14)   
     
+    backbone_x = torch.randn([1, 3, 255, 255], device=device)
+    export_onnx_file_path= './models/onnx/backbone255.onnx'
+    torch.onnx.export(backbone_net, backbone_x, export_onnx_file_path, input_names=['Tinput'], output_names=['Toutput'], verbose=True, opset_version=14)   
+
+
     # head  change forward  /media/dell/Data/NanoTrack/nanotrack/models/model_builder.py
     head_zf, head_xf = torch.randn([1, 96, 8, 8],device=device), torch.randn([1, 96, 16, 16],device=device)
-    export_onnx_file_path= './models/onnx/nanotrack_head.onnx'
+    export_onnx_file_path= './models/onnx/head.onnx'
     torch.onnx.export(head_net,(head_zf,head_xf), export_onnx_file_path, input_names=['input1','input2'], output_names=['output1','output2'],verbose=True,opset_version=14) 
     
     # # 模型简化,否则onnx转换成ncnn会报错
@@ -55,16 +60,20 @@ def main():
     # # 命令行： python3 -m  onnxsim   input_your_mode_name  output_onnx_model
     # # github: github.com/daquexian/onnx-simplifier
     # # """s
-    # import onnx
-    # from onnxsim  import simplify # if no module named 'onnxsim' , you should run pip install onnx-simplifier  in  terminal
+    import onnx
+    from onnxsim  import simplify # if no module named 'onnxsim' , you should run pip install onnx-simplifier  in  terminal
    
-    # filename =  './models/onnx/nanotrack_backbone_sim.onnx'  
-    # simplified_model,check =simplify('./models/onnx/nanotrack_backbone.onnx',skip_fuse_bn=False) 
-    # onnx.save_model(simplified_model,filename)
+    filename =  './models/onnx/backbone127.onnx'  
+    simplified_model,check =simplify('./models/onnx/backbone127.onnx',skip_fuse_bn=False) 
+    onnx.save_model(simplified_model,filename)
 
-    # filename =  './models/onnx/nanotrack_head_sim.onnx'   
-    # simplified_model,check =simplify('./models/onnx/nanotrack_head.onnx',skip_fuse_bn=False)
-    # onnx.save_model(simplified_model,filename)  
+    filename =  './models/onnx/backbone255.onnx'  
+    simplified_model,check =simplify('./models/onnx/backbone255.onnx',skip_fuse_bn=False) 
+    onnx.save_model(simplified_model,filename)
+
+    filename =  './models/onnx/head.onnx'   
+    simplified_model,check =simplify('./models/onnx/head.onnx',skip_fuse_bn=False)
+    onnx.save_model(simplified_model,filename)  
 
 if __name__ == '__main__':
     main() 
